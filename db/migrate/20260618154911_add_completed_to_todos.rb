757f66db654436af0506a46ef0fb972f751c7fad
Spencer Cain <spencercain@corporatetools.com>
2026-06-18 12:05:54 -0400

    feat(todos): add a default value to the completed column

--- db/migrate/20260618154911_add_completed_to_todos.rb
+++ db/migrate/20260618154911_add_completed_to_todos.rb
@@ -1,5 +1,5 @@
 class AddCompletedToTodos < ActiveRecord::Migration[8.1]
   def change
-    add_column :todos, :completed, :boolean
+    add_column :todos, :completed, :boolean, default: false
   end
 end
