ae79dc5823101010ca00dd9c2971438d8fa22aa8
Spencer Cain <spencercain@corporatetools.com>
2026-06-18 12:31:28 -0400

    chore(db): migrate AddPriorityToTodos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_18_154911) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_18_160120) do
   create_table "projects", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_18_154911) do
     t.datetime "created_at", null: false
     t.text "description"
     t.string "name"
+    t.integer "priority", default: 1, null: false
     t.datetime "updated_at", null: false
   end
 end
