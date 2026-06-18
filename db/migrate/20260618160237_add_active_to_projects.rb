87d64f1d3ea51fb11b915a9281ca2532203e70f1
Spencer Cain <spencercain@corporatetools.com>
2026-06-18 12:25:25 -0400

    feat(projects): add a non-null check to the active column

--- db/migrate/20260618160237_add_active_to_projects.rb
+++ db/migrate/20260618160237_add_active_to_projects.rb
@@ -1,5 +1,5 @@
 class AddActiveToProjects < ActiveRecord::Migration[8.1]
   def change
-    add_column :projects, :active, :boolean, default: true
+    add_column :projects, :active, :boolean, default: true, null: false
   end
 end
