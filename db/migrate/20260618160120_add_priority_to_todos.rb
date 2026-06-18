d83a173ac863dee234bf498fc50c868cf70a9967
Spencer Cain <spencercain@corporatetools.com>
2026-06-18 12:10:03 -0400

    feat(todos): add a non-null check to the priority column

--- db/migrate/20260618160120_add_priority_to_todos.rb
+++ db/migrate/20260618160120_add_priority_to_todos.rb
@@ -1,5 +1,5 @@
 class AddPriorityToTodos < ActiveRecord::Migration[8.1]
   def change
-    add_column :todos, :priority, :integer, default: 1
+    add_column :todos, :priority, :integer, default: 1, null: false
   end
 end
